e28cda7521fb6ff4a6a8af04bfb81c35a45751c6
Paulo Rosa <pleonrosa@gmail.com>
2016-03-03 17:08:07 -0300

    Session fim do header()
    realmente essa alternativa é melhor!

--- php/session.php
+++ php/session.php
@@ -11,14 +11,13 @@ header('Content-Type: text/html; charset=UTF-8');
 	}
 
 	require_once('php/User.php');
-	$redirecionar = "../DiarioDeClasse/index1.php";
 	session_start();
 
 	if((!isset ($_SESSION['userLogado']) == true))
 	{
 		debug_to_console("deslogou do sistema - ARQUIVO:CadastrarAluno.php");
 		unset($_SESSION['userLogado']);
-		header('location:'.$redirecionar);
+		echo '<meta HTTP-EQUIV="Refresh" CONTENT="0; URL=../DiarioDeClasse/index1.php">';
 		debug_to_console("foi redirecionado - ARQUIVO:CadastrarAluno.php");
 	}
 
